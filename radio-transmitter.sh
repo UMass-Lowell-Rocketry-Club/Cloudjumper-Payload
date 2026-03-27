1ac5ca4207ebfd1781bfcab8348c0635224b753e
Samuel Stanley <148486731+samuelstanley0101@users.noreply.github.com>
2026-03-27 18:33:10 -0400

    fixed script

--- radio-transmitter.sh
+++ radio-transmitter.sh
@@ -4,5 +4,5 @@
 # This is a script to start the radio transmitter program on startup.
 # Systemd will execute any commands in this script after the system boots.
 
-cd /home/rocketry/Cloudjumper-Payload/Cloudjumper-Sensors
+cd /home/rocketry/Cloudjumper-Sensors
 ./start_radio.sh >> radio_transmission.log
